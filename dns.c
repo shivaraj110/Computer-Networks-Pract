5e329e03121f8cbbe05afba0334628aa31cba47a
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-04-11 22:50:19 +0530

    dns implementation in c

---
+++ dns.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+int main(int argc, char *argv[]) {
+    struct hostent *hen;
+    struct in_addr **addr_list;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
+        exit(1);
+    }
+    hen = gethostbyname(argv[1]);
+    if (hen == NULL) {
+        herror("gethostbyname");
+        exit(1);
+    }
+    printf("Hostname: %s\n", hen->h_name);
+    addr_list = (struct in_addr **) hen->h_addr_list;
+    for (int i = 0; addr_list[i] != NULL; i++) {
+        printf("IP Address: %s\n", inet_ntoa(*addr_list[i]));
+    }
+    return 0;
+}
