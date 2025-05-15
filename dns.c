52e63195ece5e54f1db0a3a6c08384a2a1483e28
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-05-15 22:37:07 +0530

    enhanced code readabilty

--- dns.c
+++ dns.c
@@ -1,5 +1,4 @@
 #include <arpa/inet.h>
-#include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -7,20 +6,20 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 int main(int argc, char *argv[]) {
-  struct hostent *hen;
+  struct hostent *host;
   struct in_addr **addr_list;
 
   if (argc != 2) {
     fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
     exit(1);
   }
-  hen = gethostbyname(argv[1]);
-  if (hen == NULL) {
+  host = gethostbyname(argv[1]);
+  if (host == NULL) {
     herror("gethostbyname");
     exit(1);
   }
-  printf("Hostname: %s\n", hen->h_name);
-  addr_list = (struct in_addr **)hen->h_addr_list;
+  printf("Hostname: %s\n", host->h_name);
+  addr_list = (struct in_addr **)host->h_addr_list;
   for (int i = 0; addr_list[i] != NULL; i++) {
     printf("IP Address: %s\n", inet_ntoa(*addr_list[i]));
   }
